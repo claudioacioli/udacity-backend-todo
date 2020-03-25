c88d999018c65e7dfb83f5c36f29d3be62608306
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-25 20:07:30 -0300

    feat: Add authentication

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,14 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
+import Axios from 'axios'
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = process.env.AUTH_URL
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -54,14 +50,49 @@ export const handler = async (
   }
 }
 
+async function getKeys(url) {
+  try {
+    const { data } = await Axios.get(url);
+    return data.keys;
+  } catch(err) {
+    console.log(err)
+    throw new Error("Erro request data")
+  }
+}
+
+function getCert(x5c): string{
+  return `-----BEGIN CERTIFICATE-----\n${x5c.match(/.{1,64}/g).join('\n')}\n-----END CERTIFICATE-----\n`
+}
+
+function getPublicKey(keys): string {
+
+  if(!keys || !keys.length)
+    throw new Error("Invalid Key")
+
+  const publicKey = keys
+    .filter(key => key.use === 'sig'
+      && key.kty === 'RSA'
+      && key.kid
+      && ((key.x5c && key.x5c.length) || (key.n && key.e))
+    ).map(key => getCert(key.x5c[0]))
+  
+  if (!publicKey.length)
+    throw new Error("Invalid Public Key")
+
+  return publicKey[0];
+}
+
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt	
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if(!jwt)
+    throw new Error("Invalid token")
+
+  const keys = await getKeys(jwksUrl);
+  const publicKey = getPublicKey(keys);
+  const payload = verify(token, publicKey, { algorithms: ['RS256'] })
+  return payload as JwtPayload
 }
 
 function getToken(authHeader: string): string {
