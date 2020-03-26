096630e55b295195e8b0c73d3317429a515f54f0
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-26 11:49:15 -0300

    refactor: Implements function

--- src/lambda/http/deleteTodo.ts
+++ src/lambda/http/deleteTodo.ts
@@ -1,10 +1,19 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { deleteTodo } from '../../businessLogic/todo'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
   const todoId = event.pathParameters.todoId
+  await deleteTodo(todoId)
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true 
+    },
+    body: null
+  }
 }
