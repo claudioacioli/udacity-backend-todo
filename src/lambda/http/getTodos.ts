03cdfe8ec2ae0a23426c5977e94d16613e6e71ea
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-26 02:54:59 -0300

    feat: Implement list of todos

---
+++ src/lambda/http/getTodos.ts
@@ -0,0 +1,25 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getTodosByUser } from '../../businessLogic/todo'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
+  const jwtToken = getToken(event.headers.Authorization)
+  const todos = await getTodosByUser(jwtToken)
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todos
+    })
+  }
+
+}
+
+function getToken(header: string) {
+  return header.split(" ")[1]
+}
