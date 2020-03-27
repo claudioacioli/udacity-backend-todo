87c92a289c06a04547c6a86ed68cf174b14de5b4
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-27 04:14:19 -0300

    refactor: Use layers access

--- src/lambda/http/generateUploadUrl.ts
+++ src/lambda/http/generateUploadUrl.ts
@@ -1,21 +1,12 @@
 import 'source-map-support/register'
-import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-
-const bucketName = process.env.TODOS_IMAGES_S3_BUCKET
-const urlExpiration = 300 //process.env.TODOS_SIGNED_URL_EXPIRATION
-
-const s3 = new AWS.S3({
-  signatureVersion: 'v4'
-})
+import { getUploadUrl } from '../../businessLogic/s3'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
   const todoId = event.pathParameters.todoId
-
-  console.log("bucketName", bucketName)
-  console.log("urlExpiration", urlExpiration)
-
   const url = getUploadUrl(todoId)
+
   return {
     statusCode: 201,
     headers: {
@@ -27,10 +18,3 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   }
 }
 
-function getUploadUrl(todoId: string): string {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: todoId,
-    Expires: urlExpiration
-  }) as string
-}
