c4b87bb5cc75378ea25a7d847bf99e870b186552
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-27 05:08:07 -0300

    feat: Add business layer to verify attachment

--- src/businessLogic/s3.ts
+++ src/businessLogic/s3.ts
@@ -2,6 +2,10 @@ import { S3Access } from '../dataLayer/s3Access'
 
 const s3Access = new S3Access()
 
+export async function getAttachmentUrl(todoId: string): Promise<boolean> {
+  return await s3Access.getAttachmentUrl(todoId)
+}
+
 export function getUploadUrl(todoId: string): string {
   return s3Access.getUploadUrl(todoId)
 }
