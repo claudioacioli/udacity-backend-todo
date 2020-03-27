3e46593e25e64113554cbef42523902c13b06253
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-03-26 22:44:15 -0300

    chore: Implement and ddeploy function

--- src/lambda/http/generateUploadUrl.ts
+++ src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,36 @@
 import 'source-map-support/register'
-
+import * as AWS from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+const bucketName = process.env.TODOS_IMAGES_S3_BUCKET
+const urlExpiration = 300 //process.env.TODOS_SIGNED_URL_EXPIRATION
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+})
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  console.log("bucketName", bucketName)
+  console.log("urlExpiration", urlExpiration)
+
+  const url = getUploadUrl(todoId)
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
+}
+
+function getUploadUrl(todoId: string){
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: todoId,
+    Expires: urlExpiration
+  })
 }
